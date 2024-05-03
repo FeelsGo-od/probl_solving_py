93167439610c434d6463efcdc7886a50b5d5009b
FeelsGo-od <andrew.sokolowsky@gmail.com>
2024-05-03 07:53:31 +0200

    add access env variables

---
+++ access_env_vars/main.py
@@ -0,0 +1,12 @@
+import os
+import pprint
+
+# print(os.environ['PATH'])
+
+# # Add a new environment variable  
+# os.environ['TestVar'] = 'somevalue'
+  
+# # Get the value  
+# print("TestVar:", os.environ['TestVar']) 
+
+# pprint.pprint(dict(os.environ), width = 1)
